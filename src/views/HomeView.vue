bbbe7102b75083eb03c07aad8ad1b60978c65730
pollux <artemgimranov57@gmail.com>
2025-12-05 21:19:53 +0400

    fix scroll in home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -155,7 +155,7 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <section class="h-full w-full flex flex-col overflow-hidden">
+  <section class="h-full w-full flex flex-col overflow-hidden overflow-y-auto">
 
     <SearchField v-model="searchQuery" :placeholder="$t('pages.index.searchPlaceholder')"
       @search-change="debouncedSearch" class="w-full" />
