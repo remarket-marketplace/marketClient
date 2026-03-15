6a16369ba2e4997a0eb810844a70c61808f7217b
Lansqe <pisarev.orig@mail.ru>
2026-03-15 18:12:59 +0300

    Дата бар

--- src/i18n/locales/ru.ts
+++ src/i18n/locales/ru.ts
@@ -675,6 +675,8 @@ const ru = {
       refundConfirmMessage: "Вы уверены что хотите сделать возврат по этой сделке?",
       report: "Пожаловаться",
       messagePlaceholder: "Введите сообщение...",
+      today: "сегодня",
+      yesterday: "вчера",
       waitReplyReminderSender: "Чтобы продолжить общение, дождитесь ответа собеседника.",
       waitReplyReminderRecipient: "Пользователь сможет отправлять вам сообщения только после вашего ответа.",
       newPurchase: "Новая покупка",
