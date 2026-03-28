640e070047f2d66deb6a0c111da03e9c6b9af649
pollux <artemgimranov57@gmail.com>
2026-03-28 17:04:58 +0400

    update validation errors text color && fields validation failed -> button not active

--- src/views/SignUp.vue
+++ src/views/SignUp.vue
@@ -413,7 +413,7 @@ function handleWelcomeFinished() {
             <TheInput id="username" v-model="username" type="text"
               placeholder="" required @blur="validateUsername"
               @input="clearUsernameError" :minlength="4" :maxlength="32" autocomplete="username" />
-            <p v-if="usernameError" class="text-gray-400 text-xs leading-4 mt-1">{{ usernameError }}</p>
+            <p v-if="usernameError" class="mt-1 text-xs leading-4 text-red-300">{{ usernameError }}</p>
           </div>
 
           <!-- Email -->
@@ -421,7 +421,7 @@ function handleWelcomeFinished() {
             <label for="email" class="mb-1 block text-sm text-text-secondary">{{ $t('common.email') }}</label>
             <TheInput id="email" v-model="email" type="email" placeholder="" required
               @blur="validateEmail" @input="clearEmailError" :maxlength="64" autocomplete="email" />
-            <p v-if="emailError" class="text-gray-400 text-xs leading-4 mt-1">{{ emailError }}</p>
+            <p v-if="emailError" class="mt-1 text-xs leading-4 text-red-300">{{ emailError }}</p>
           </div>
 
           <!-- Password с иконкой глаза -->
@@ -438,7 +438,7 @@ function handleWelcomeFinished() {
               </template>
             </TheInput>
             <div v-if="password.length > 0 && activePasswordHint" class="mt-2">
-              <p class="flex items-center gap-2 text-xs leading-4 text-gray-400">
+              <p class="flex items-center gap-2 text-xs leading-4 text-red-300">
                 <span class="inline-flex w-3 justify-center font-semibold">•</span>
                 <span>{{ activePasswordHint.label }}</span>
               </p>
