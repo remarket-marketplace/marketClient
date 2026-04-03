6a80d2c3603516a16ee1f9acfbf5b3e285412e40
pollux <artemgimranov57@gmail.com>
2026-04-03 23:06:58 +0400

    fix: prioritize product image modal close button

--- src/views/ProductPage.vue
+++ src/views/ProductPage.vue
@@ -970,8 +970,8 @@ onUnmounted(() => {
             :alt="`Modal image: ${product.title}`" loading="lazy" />
 
           <button
-            class="absolute top-4 right-4 text-white hover:text-gray-300 transition-all duration-200 bg-black/50 rounded-full p-2 hover:bg-black/70"
-            @click="openImageModal = false">
+            class="absolute top-4 right-4 z-30 text-white hover:text-gray-300 transition-all duration-200 bg-black/50 rounded-full p-2 hover:bg-black/70"
+            @click.stop="openImageModal = false">
             <X class="w-6 h-6" />
           </button>
 
