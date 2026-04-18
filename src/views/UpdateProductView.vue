53b0d4ca2b9fcce5a280160fb60ee3f46a3a6cc5
Lansqe <pisarev.orig@mail.ru>
2026-04-18 15:00:58 +0300

    feat(products): increase title length validation to 80

--- src/views/UpdateProductView.vue
+++ src/views/UpdateProductView.vue
@@ -48,7 +48,7 @@ const currencySymbol = computed(() => getCurrencySymbol(selectedCurrency.value))
 const usdRubRate = computed(() => getUsdRubRate())
 
 const PRODUCT_LIMITS = {
-  title: { min: 10, max: 50 },
+  title: { min: 10, max: 80 },
   description: { min: 10, max: 1200 },
   productData: { min: 10, max: 128 },
   count: { min: 1, max: 5000 },
